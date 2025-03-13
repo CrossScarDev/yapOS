5bc6e75547d017de140167be6b70723eefa5e928
Grady Link <grady.link@grady.link>
2025-03-12 17:58:59 -0700

    switch to use gridview

--- launcher/src/main.lua
+++ launcher/src/main.lua
@@ -2,8 +2,10 @@ import("CoreLibs/object")
 import("CoreLibs/graphics")
 import("CoreLibs/sprites")
 import("CoreLibs/timer")
+import("CoreLibs/ui")
 
-local gfx = playdate.graphics
+local gfx <const> = playdate.graphics
+local ui <const> = playdate.ui
 
 local games = {}
 for _, group in ipairs(playdate.datastore.read("/System/Data/games")) do
@@ -12,51 +14,54 @@ for _, group in ipairs(playdate.datastore.read("/System/Data/games")) do
 	end
 end
 
-local selectedGame = 1
-
 local font = gfx.font.new("fonts/roobert11")
 gfx.setFont(font)
 
-local function render()
-	gfx.clear()
-
-	local y = 20
-	for i, game in ipairs(games) do
-		gfx.setImageDrawMode(playdate.graphics.kDrawModeFillBlack)
-		if i == selectedGame then
-			gfx.setColor(playdate.graphics.kColorBlack)
-			gfx.fillRoundRect(
-				10,
-				y - 10,
-				font:getTextWidth(game["title"]) + 30,
-				font:getHeight() + 20,
-				(font:getHeight() + 20) / 2
-			)
-			gfx.setImageDrawMode(playdate.graphics.kDrawModeFillWhite)
-		end
-		gfx.drawText(game["title"], 25, y)
-		y += font:getHeight() + 22.5
+local listview = ui.gridview.new(playdate.display.getWidth() / 2, font:getHeight() + 20)
+listview:setNumberOfRows(#games)
+listview:setNumberOfColumns(1)
+listview:setCellPadding(0, 0, 2.5, 2.5)
+listview:setContentInset(5, 0, 5, 0)
+listview:setSelectedRow(1)
+function listview:drawCell(section, row, column, selected, x, y, width, height)
+	gfx.setImageDrawMode(playdate.graphics.kDrawModeFillBlack)
+	if selected then
+		gfx.setColor(gfx.kColorBlack)
+		gfx.fillRoundRect(x, y, width, height, height / 2)
+		gfx.setImageDrawMode(playdate.graphics.kDrawModeFillWhite)
 	end
+	gfx.drawTextInRect(
+		games[row]["title"],
+		x,
+		y + height / 2 - font:getHeight() / 2,
+		width,
+		height,
+		nil,
+		nil,
+		kTextAlignment.center
+	)
+end
+
+function playdate.update()
+	gfx.clear()
+	playdate.timer.updateTimers()
+	listview:drawInRect(0, 0, playdate.display.getWidth(), playdate.display.getHeight())
 end
 
 function playdate.downButtonDown()
-	if selectedGame < #games then
-		selectedGame += 1
-		render()
+	if listview:getSelectedRow() < #games then
+		listview:setSelectedRow(listview:getSelectedRow() + 1)
+		listview:scrollCellToCenter(1, listview:getSelectedRow(), 1)
 	end
 end
 
 function playdate.upButtonDown()
-	if selectedGame > 1 then
-		selectedGame -= 1
-		render()
+	if listview:getSelectedRow() > 1 then
+		listview:setSelectedRow(listview:getSelectedRow() - 1)
+		listview:scrollCellToCenter(1, listview:getSelectedRow(), 1)
 	end
 end
 
 function playdate.AButtonDown()
-	playdate.system.switchToGame(games[selectedGame]["path"])
+	playdate.system.switchToGame(games[listview:getSelectedRow()]["path"])
 end
-
-render()
-
-function playdate.update() end
