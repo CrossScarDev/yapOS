4a3e1ad25f0e65b630e3d038418b9cde0bca9f16
Grady Link <grady.link@grady.link>
2025-03-15 16:10:41 -0700

    Installer: correct downloadSteps constant

--- installer/frontend/src/global.ts
+++ installer/frontend/src/global.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue';
 
 export const step = ref(1);
 
-export const downloadSteps = 3
+export const downloadSteps = 4
 export const installSteps = 0
 
 export const canContinue = ref(true);
